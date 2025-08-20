d520fdb4be3e94dbfdf596bdf02119a689f4947a
devkarx <aradhydeokar@gmail.com>
2025-08-20 16:33:49 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ dotenv.config({
 
 connectDB()
 .then(() => {
+    app.on("error", (error)=>{
+            console.log("ERR: ", error);
+            throw error;    
+        })  
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
